b35adefb6824b930c202c3063c9b6a825ca2687d
Novian <muhammad1700018086@webmail.uad.ac.id>
2020-06-28 20:51:41 +0700

    first commit

---
+++ form-1.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title></title>
+</head>
+
+<body>
+
+    Selamat datang : <?php echo $_POST["name"]; ?><br>
+    Alamat anda : <?php echo $_POST["alamat"]; ?><br>
+    Telepon anda : <?php echo $_POST["telepon"]; ?><br>
+    Jenis Kelamin anda : <?php echo $_POST["jk"]; ?><br>
+    Agama anda : <?php echo $_POST["agama"]; ?><br>
+    Prestasi anda : <?php echo $_POST["Prestasi"]; ?><br>
+</body>
+
+</html>
